0f1eec83a6e19b75d034b45f659d9fb60d632490
David <shulga@ucoz-team.net>
2021-05-31 15:57:27 +0300

    new test2

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,8 +1,9 @@
 const assert = require('assert')
 
 describe('quarkly.io page', () => {
-    it('should return something for', async () => {
-        await browser.url('https://quarkly.io')
+    it('should return something for', () => {
+        browser.url('https://quarkly.io')
+
         expect(browser).toHaveTitle('Quarkly – Design tool for creating websites and web apps.')
     });   
 });
